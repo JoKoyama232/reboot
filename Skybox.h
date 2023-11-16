8e542000edabf4b1ecafb428163d1bd958fcaf1c
jokoyama <hal.koyama.jo@gmail.com>
2023-11-16 16:31:54 +0900

    空と光を追加

--- Skybox.h
+++ Skybox.h
@@ -2,24 +2,10 @@
 // Author : 小山　城
 //=============================================================================
 #pragma once
-// インクルード
-#include "model.h"
-#include "WorldSpaceClass.h"
 
-
-// 構造定義
-struct Skybox {
-	// ワールド位置情報
-	WorldSpace transform;
-
-	// 読み込み状況管理
-	bool use = false;
-	bool load = false;
-
-};
 
 // 関数群
 HRESULT InitSkybox(void);
 void UninitSkybox(void);
-void UpdatSkybox(void);
+void UpdateSkybox(void);
 void DrawSkybox(void);
